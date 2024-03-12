cd2e4b01ddd69d85c42c0396bb3b96e4ffb607a0
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-12 19:16:57 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/main/ExercicioForIntervalo.java
@@ -0,0 +1,14 @@
+package com.ruanmattos.main;
+
+/**
+ * Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do
+ * intervalo, mostrando essas informações conforme exemplo (use a palavra "dentro do intervalo" para dentro do intervalo, e "fora do intervalo" para fora do intervalo).
+ *
+ * @author Ruan Pereira Mattos
+ */
+public class ExercicioForIntervalo {
+
+    public static void main(String[] args) {
+        
+    }
+}
